c58993b3bc3fb0fc8bbba9a43a2da895045567b2
unknown <hhg.mzt@gmail.com>
2020-06-21 19:38:29 -0600

    Updates

---
+++ precache-manifest.35e1b83c1985b5d503b8e435fa884c87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246b83a51d120855af621de4f7233ed0",
+    "url": "/HiramHernandez/calculator/index.html"
+  },
+  {
+    "revision": "60e8ecafcc05cd05a627",
+    "url": "/HiramHernandez/calculator/static/css/main.0f98e074.chunk.css"
+  },
+  {
+    "revision": "96e87d43e9514494a063",
+    "url": "/HiramHernandez/calculator/static/js/2.5df6ed0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HiramHernandez/calculator/static/js/2.5df6ed0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e8ecafcc05cd05a627",
+    "url": "/HiramHernandez/calculator/static/js/main.1d270107.chunk.js"
+  },
+  {
+    "revision": "2c8a47b6487478bd6546",
+    "url": "/HiramHernandez/calculator/static/js/runtime-main.bfe82f4b.js"
+  }
+]);
